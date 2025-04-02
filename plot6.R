291217090e477a4519246c93e79eaaf362ffe736
Ross England <rossengland@gmail.com>
2025-04-02 14:30:49 -0400

    Added annotation at top to indicate purpose of code.
    Added code to read relevant data files.

---
+++ plot6.R
@@ -0,0 +1,14 @@
+#Use a plotting system of your choice to compare emissions over time from motor 
+#vehicle sources in Baltimore City (fips == "24510") and Los Angeles County 
+#(fips == "06037")
+
+#fips = a five-digit number indicating the US county
+#SCC = the name of the source as indicated by a digit string
+#Pollutant = a string indicating the polluttant 
+#Emissions = amount of PM2.5 emitted in tons
+#type: the type of source (point, non-point, on-road, non-road)
+#year: The year of emissions recorded
+
+#Read data files
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
