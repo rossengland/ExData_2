4f7ac20736218a706097b936065b86863a32f13d
Ross England <rossengland@gmail.com>
2025-04-02 14:27:02 -0400

    Added annotation at top to indicate purpose of code.
    Added code to read relevant data files.

---
+++ plot4.R
@@ -0,0 +1,13 @@
+#Use a plotting system of your choice to determine how emissions from 
+#coal-combustion related sources changed from 1999 - 2008
+
+#fips = a five-digit number indicating the US county
+#SCC = the name of the source as indicated by a digit string
+#Pollutant = a string indicating the polluttant 
+#Emissions = amount of PM2.5 emitted in tons
+#type: the type of source (point, non-point, on-road, non-road)
+#year: The year of emissions recorded
+
+#Read data files
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
