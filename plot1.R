07a7e555e67b0d16f04ea240168847b5532d8f57
Ross England <rossengland@gmail.com>
2025-04-02 14:16:47 -0400

    Added annotation at the top indicating purpose of the code.
    Added code to read in relevant data files.

--- plot1.R
+++ plot1.R
@@ -1,2 +1,6 @@
-print("This file was created within RStudio")
-print("And now it lives on Github")
+#Use base plotting system to make a plot showing the total PM2.5
+#emission from all sources for each of the years 1999,2002,2005,2008
+
+#Read data files
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
